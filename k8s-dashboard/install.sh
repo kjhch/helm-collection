b07459e855edc703c70f14be657f982978bf3102
hch <kejun.hch@qq.com>
2021-09-03 20:56:40 +0800

    fix: dashboard

--- k8s-dashboard/install.sh
+++ k8s-dashboard/install.sh
@@ -1,6 +1,13 @@
 CWD=$(cd "$(dirname "$0")";pwd)
+NAMESPACE="kube-system"
+METRICS_SERVER_VERSION="4.1.10"
 VERSION="2.6.0"
+
+
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo update
+helm install metrics-server bitnami/metrics-server --version $METRICS_SERVER_VERSION -n $NAMESPACE
 
 helm repo add k8s-dashboard https://kubernetes.github.io/dashboard
 helm repo update
-helm install kubernetes-dashboard k8s-dashboard/kubernetes-dashboard --version $VERSION --values $CWD/values.yml -n kube-system
+helm install kubernetes-dashboard k8s-dashboard/kubernetes-dashboard --version $VERSION --values $CWD/values.yml -n $NAMESPACE
