4223c02c6971bf287faa5efdc98314dc11aaf0de
hch <kejun.hch@qq.com>
2021-09-03 20:39:15 +0800

    fix: dashboard

--- k8s-dashboard/install.sh
+++ k8s-dashboard/install.sh
@@ -3,4 +3,4 @@ VERSION="2.6.0"
 
 helm repo add k8s-dashboard https://kubernetes.github.io/dashboard
 helm repo update
-helm install my-kubernetes-dashboard k8s-dashboard/kubernetes-dashboard --version $VERSION --values $CWD/values.yml -n kube-system
+helm install kubernetes-dashboard k8s-dashboard/kubernetes-dashboard --version $VERSION --values $CWD/values.yml -n kube-system
