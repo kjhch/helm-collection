b38b9b75cf477dacc243a254ddafd706d9381f4b
hch <kejun.hch@qq.com>
2021-09-05 14:15:55 +0800

    fix: springboot bench

--- springboot-bench/run.sh
+++ springboot-bench/run.sh
@@ -1,14 +1,17 @@
-CWD=$(cd "$(dirname "$0")";pwd)
 NAMESPACE="app"
 ACTION="install"
+CWD=$(cd "$(dirname "$0")";pwd)
+ADDITIONAL_FLAGS=$*
+
+
 if [[ "$1" == "u" || "$1" == "upgrade" ]]; then
     ACTION="upgrade"
     shift 1
+    ADDITIONAL_FLAGS="$* --reset-values"
 fi
 echo "helm action: $ACTION"
 
 helm $ACTION `basename $CWD` $CWD \
 -n $NAMESPACE \
 --create-namespace \
---reset-values \
-$*
+$ADDITIONAL_FLAGS
