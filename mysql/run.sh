e3466326a47a586679772b48fe1070d6a043f46e
hch <kejun.hch@qq.com>
2021-09-12 08:46:28 +0800

    fix: mysql

--- mysql/run.sh
+++ mysql/run.sh
@@ -1,17 +1,23 @@
+set -x
+
 CWD=$(cd "$(dirname "$0")";pwd)
 NAMESPACE="share"
 VERSION="6.14.12"
 ACTION="install"
-if [[ "$1" == "u" || "$1" == "upgrade" ]]
-then ACTION="upgrade"
-fi
-echo "action: $ACTION"
+ADDITIONAL_FLAGS=$*
 
+if [[ "$1" == "u" || "$1" == "upgrade" ]]; then
+    ACTION="upgrade"
+    shift 1
+    ADDITIONAL_FLAGS="$* --reset-values"
+fi
+echo "helm action: $ACTION"
 
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
-helm $ACTION mysql bitnami/mysql \
+helm $ACTION `basename $CWD` $CWD \
 -n $NAMESPACE \
 --create-namespace \
 --version $VERSION \
---values $CWD/values.yml
+--values $CWD/values.yml \
+$ADDITIONAL_FLAGS
