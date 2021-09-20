b65b67b625b56d7315394b15eef52232ca8dd57d
hch <kejun.hch@qq.com>
2021-09-20 14:00:50 +0800

    feat: apm

--- skywalking/run.sh
+++ skywalking/run.sh
@@ -1,22 +1,15 @@
 set -x
 CWD=$(cd "$(dirname "$0")";pwd)
 NAMESPACE="share"
-VERSION="4.1.0"
-ACTION="install"
-ADDITIONAL_FLAGS=$*
-
-if [[ "$1" == "u" || "$1" == "upgrade" ]]; then
-    ACTION="upgrade"
-    shift 1
-    ADDITIONAL_FLAGS="$* --reset-values"
-fi
-echo "helm action: $ACTION"
+VERSION="4.0.0"
 
 helm repo add skywalking https://apache.jfrog.io/artifactory/skywalking-helm
 helm repo update
-helm $ACTION `basename $CWD` skywalking/skywalking \
+helm upgrade `basename $CWD` skywalking/skywalking \
+--install
 -n $NAMESPACE \
 --create-namespace \
 --version $VERSION \
 --values $CWD/values.yml \
-$ADDITIONAL_FLAGS
+$* \
+--reset-values 
