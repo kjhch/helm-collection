fc5596a5e97f8f672fb51bf5a804d007343b76ac
hch <kejun.hch@qq.com>
2021-09-20 09:27:12 +0800

    feat: apm

---
+++ apm-server/run.sh
@@ -0,0 +1,22 @@
+set -x
+CWD=$(cd "$(dirname "$0")";pwd)
+NAMESPACE="share"
+VERSION="7.14.0"
+ACTION="install"
+ADDITIONAL_FLAGS=$*
+
+if [[ "$1" == "u" || "$1" == "upgrade" ]]; then
+    ACTION="upgrade"
+    shift 1
+    ADDITIONAL_FLAGS="$* --reset-values"
+fi
+echo "helm action: $ACTION"
+
+helm repo add elastic https://helm.elastic.co
+helm repo update
+helm $ACTION `basename $CWD` elastic/apm-server \
+-n $NAMESPACE \
+--create-namespace \
+--version $VERSION \
+--values $CWD/values.yml \
+$ADDITIONAL_FLAGS
