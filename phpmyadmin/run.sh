d4f37b73dcb923ce195dd612e9149639cf4eb90b
科君 <huangchenhui.hch@cainiao.com>
2023-01-17 17:16:04 +0800

    feat: mysql

---
+++ phpmyadmin/run.sh
@@ -0,0 +1,22 @@
+set -x
+CWD=$(cd "$(dirname "$0")";pwd)
+NAMESPACE="share"
+VERSION="10.4.0"
+ACTION="install"
+ADDITIONAL_FLAGS=$*
+
+if [[ "$1" == "u" || "$1" == "upgrade" ]]; then
+    ACTION="upgrade"
+    shift 1
+    ADDITIONAL_FLAGS="$* --reset-values"
+fi
+echo "helm action: $ACTION"
+
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo update
+helm $ACTION `basename $CWD` bitnami/phpmyadmin \
+-n $NAMESPACE \
+--create-namespace \
+--version $VERSION \
+--values $CWD/values.yml \
+$ADDITIONAL_FLAGS
